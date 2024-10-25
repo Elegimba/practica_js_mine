ee57bcc1a36739c5cea74a9831591eba1015b547
Juan Castaneda <jccastanedacamargo@outlook.com>
2024-10-25 23:06:36 +0200

    Se crea pintado de de la sección de escaparate, para inyectado en el index.

---
+++ js/funcionalidades.js
@@ -0,0 +1,40 @@
+//Capturas de elementos del DOM
+
+const productSection = document.querySelector('.escaparate');
+
+
+
+// Pintar productos en el Screen.
+function printOneArticle(articulo, dom) {
+    // Creación de los elementos
+    const article = document.createElement('article');
+    const figure = document.createElement('figure');
+    const img = document.createElement('img');
+    const h3 = document.createElement('h3');
+    const ul = document.createElement('ul');
+    const btn = document.createElement('button');
+
+    // Meter contenido y gestionar atributos
+    img.src = articulo.imagen;
+    img.alt = articulo.nombre;
+    h3.textContent = articulo.nombre;
+    ul.innerHTML = `<li>${articulo.descripcion}</li>
+                    <li>Precio: ${articulo.precio}</li>`;
+    btn.textContent = "Agregar al carrito";
+
+    // Meter los elementos dentro de sus contenedores
+    figure.appendChild(img);
+    article.append(figure, h3, ul, btn);
+    dom.appendChild(article);
+}
+
+
+function printAllStock(stockList, dom) {
+    stockList.forEach(articulo => printOneArticle(articulo, dom))
+    
+    
+}
+
+// Termina seccion de pintar productos
+
+console.log(printAllStock(productList, productSection));
